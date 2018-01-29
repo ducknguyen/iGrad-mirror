97bedec57cb1ce917c6bbb8a85c782a662f30b61
JEFF <J.PRATT3991@GMAIL.COM>
2018-01-28 18:45:30 -0800

    Added initial landing page and site design.

---
+++ IGrad/IGrad/CustomHelpers/CustomHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IGrad.CustomHelpers
+{
+    public static class CustomHelper
+    {
+        public static IHtmlString viewCard(this HtmlHelper helper, int rows)
+        {
+            string htmlStr = @"<div class=""well well-lg""rows=" + rows + "></div>";
+            return new MvcHtmlString(htmlStr);
+        }
+
+        public static IHtmlString buttonSubmit(this HtmlHelper helper, string buttonText, string cssFormatting)
+        {
+            return new MvcHtmlString("<input type=\"submit\" class=\"btn " + cssFormatting + "\" value=\"" + buttonText + "\"/>");
+        }
+
+    }
+}
