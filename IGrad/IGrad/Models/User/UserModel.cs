9846dae75ba2a0f596a4a35efd80ad36e6acd98e
kid101skater <kevin.locke90@gmail.com>
2018-02-02 20:17:23 -0800

    Fix usermodel update

--- IGrad/IGrad/Models/User/UserModel.cs
+++ IGrad/IGrad/Models/User/UserModel.cs
@@ -7,6 +7,23 @@ namespace IGrad.Models.User
 {
     public class UserModel
     {
-                
+        public DateTime Birthday { get; set; }
+        public BirthPlaceLocation BirthPlace { get; set; }
+        public LoginViewModel LoginInfo { get; set; }
+        public Address ResidentAddress { get; set; }
+        public Address MailingAddress { get; set; }
+        public HomePhone PhoneObject { get; set; }
+        public ParentPlan StudentsParentingPlan { get; set; }
+        public Sibling[] Siblings { get; set; }
+        public ChildCare StudentChildCare { get; set; }
+        public PreSchools[] PreSchool { get; set; }
+        public QualifiedOrEnroledPrograms[] QualifiedOrEnrolledProgam { get; set; }
+        public Retained Retainment { get; set; }
+        public EmergencyContact[] EmergencyContacts { get; set; }
+        public RaceEthnicity ConsideredRaceAndEthnicity { get; set; }
+        public string CountryBornIn { get; set; }
+        public SchoolInfo HighSchoolInfo { get; set; }
+        public Health HealthInfo { get; set; }
+
     }
 }
