d9e74755313e87a4ee3d5fd778cceb303dea7f8e
JEFF <J.PRATT3991@GMAIL.COM>
2018-02-02 21:33:07 -0800

    Bound the user model to front page view.

--- IGrad/IGrad/Models/User/Name/Name.cs
+++ IGrad/IGrad/Models/User/Name/Name.cs
@@ -1,11 +1,18 @@
-﻿namespace IGrad.Models.User
+﻿using System.ComponentModel;
+
+namespace IGrad.Models.User
 {
     public class Name
     {
+        [DisplayName("First Name")]
         public string FName { get; set; }
+        [DisplayName("Last Name")]
         public string LName { get; set; }
+        [DisplayName("Middle Name")]
         public string MName { get; set; }
+        [DisplayName("Nick Name")]
         public string NickName { get; set; }
+        [DisplayName("Previous Name")]
         public string PreviousName { get; set; }
     }
 }
--- IGrad/IGrad/Models/User/RaceEthnicity/RaceEthnicity.cs
+++ IGrad/IGrad/Models/User/RaceEthnicity/RaceEthnicity.cs
@@ -1,16 +1,28 @@
-﻿namespace IGrad.Models.User
+﻿using System.ComponentModel;
+
+namespace IGrad.Models.User
 {
     public class RaceEthnicity
     {
+        [DisplayName("Not Hispanic Or Latino")]
         public bool isNotHispanicOrLatino { get; set; }
+        [DisplayName("Cuban")]
         public bool isCuban { get; set; }
-        public bool isDominican { get; set;}
+        [DisplayName("Dominican")]
+        public bool isDominican { get; set; }
+        [DisplayName("Spanish")]
         public bool isSpaniard { get; set; }
+        [DisplayName("Puerto RIcan")]
         public bool isPuertoRican { get; set; }
+        [DisplayName("Mexican")]
         public bool isMexican { get; set; }
+        [DisplayName("Central American")]
         public bool isCentralAmerican { get; set; }
+        [DisplayName("South American")]
         public bool isSouthAmerican { get; set; }
+        [DisplayName("Latin American")]
         public bool isLatinAmerican { get; set; }
+        [DisplayName("Other Hispanic / Latino")]
         public bool isOtherHispanicLatino { get; set; }
 
     }
