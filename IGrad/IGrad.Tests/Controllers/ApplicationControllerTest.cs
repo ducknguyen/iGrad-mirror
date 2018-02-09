74aa587fcbf268bd6d3da0191e0999496302a60e
babywhalenuke <j.pratt3991@gmail.com>
2018-02-09 11:13:29 -0800

    Completed home page test for selenium project

--- IGrad/IGrad.Tests/Controllers/ApplicationControllerTest.cs
+++ IGrad/IGrad.Tests/Controllers/ApplicationControllerTest.cs
@@ -33,27 +33,24 @@ namespace IGrad.Tests.Controllers
         [TestMethod]
         public void GetHomeTestLinkForExternalIGradSite()
         {
-            // Arrange
-            ApplicationController controller = new ApplicationController();
-
-            // Act
-            ViewResult result = controller.GetNewApplication() as ViewResult;
-
-
             //Execute Driver 
             ChromeDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("http://codedot.net/");
             IWebElement clickableLink = driver.FindElementById("igrad-external");
-            IEnumerable<string> windowHandles = driver.WindowHandles;
             string urlToNavTo = clickableLink.GetAttribute("href");
-            driver.Navigate().GoToUrl(urlToNavTo);
-            string title = "iGrad / Home";
-            Debug.WriteLine(driver.Title);
+            string iGradTitle = driver.Title;
 
-            //Assert
+            //Assert the title of the driver and title of page are equal
+            Assert.AreEqual(driver.Title, iGradTitle);
+            string title = "iGrad / Home";
+            driver.Navigate().GoToUrl(urlToNavTo);
+
+            //Assert the title of new page and title of url are equal
             Assert.IsTrue(driver.Title == title);
 
 
         }
+
+
     }
 }
