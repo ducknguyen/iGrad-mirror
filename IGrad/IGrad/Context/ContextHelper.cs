687440a9f328f5c1138587e8c57e2b3aa8a37b64
kid101skater <kevin.locke90@gmail.com>
2018-02-11 12:37:23 -0800

    Updated Context to be static

--- IGrad/IGrad/Context/ContextHelper.cs
+++ IGrad/IGrad/Context/ContextHelper.cs
@@ -6,7 +6,7 @@ namespace IGrad.Context
 {
     public class ContextHelper
     {
-        public void UpdateRecord(UserModel updatedUser)
+        public static void UpdateRecord(UserModel updatedUser)
         {
             try
             {
@@ -23,6 +23,27 @@ namespace IGrad.Context
             catch
             {
                 // TODO: what we wanna do here?
+            }
+        }
+
+        public static UserModel GetUserInfo(Guid userId)
+        {
+            try
+            {
+                using (UserContext db = new UserContext())
+                {
+                    var result = db.Users.SingleOrDefault(u => u.UserID == userId);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                    else return null;
+                }
+            }
+            catch
+            {
+                // TODO: what we wanna do here?
+                return null;
             }
         }
     }
