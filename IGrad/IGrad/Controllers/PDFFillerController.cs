85eac055db178c25b593e3f03269b0a8077efbc0
JEFF <J.PRATT3991@GMAIL.COM>
2018-02-11 13:55:05 -0800

    Added pdf control and testing for pdf fillout

--- IGrad/IGrad/Controllers/PDFFillerController.cs
+++ IGrad/IGrad/Controllers/PDFFillerController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using IGrad.Models.User;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.AcroForms;
+using PdfSharp.Pdf.IO;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +17,39 @@ namespace IGrad.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        
+
+        public ActionResult FillPdf(UserModel user)
+        {
+            PdfDocument document = PdfReader.Open("C:/Users/jpratt/Documents/GitHub/igrad/IGrad/IGrad/media/documents/iGradFirstForm.pdf");
+
+            PdfTextField firstName = (PdfTextField)(document.AcroForm.Fields["StudentFirstName"]);
+            firstName.Value = new PdfString(user.Name.FName);
+
+            PdfTextField lastName = (PdfTextField)(document.AcroForm.Fields["StudentLastName"]);
+            firstName.Value = new PdfString(user.Name.LName);
+
+            PdfTextField middleInitial = (PdfTextField)(document.AcroForm.Fields["MI"]);
+            middleInitial.Value = new PdfString(user.Name.MName);
+            
+            PdfTextField birthday = (PdfTextField)(document.AcroForm.Fields["Birthday"]);
+            birthday.Value = new PdfString(user.Birthday.ToString());
+
+            document.Save("C:/Users/jpratt/Documents/GitHub/igrad/IGrad/IGrad/media/documents/iGradComplete.pdf");
+
+            return null;
+
+            //MemoryStream stream = new MemoryStream();
+            //document.Save(stream, false);
+            //Response.Clear();
+            //Response.ContentType = "application/pdf";
+            //Response.AddHeader("content-length", stream.Length.ToString());
+            //Response.BinaryWrite(stream.ToArray());
+            //Response.Flush();
+            //stream.Close();
+            //Response.End();
+
         }
     }
 }
