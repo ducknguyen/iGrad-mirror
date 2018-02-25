3aca5ac092c86cd8fbc896ad78258da6a1de7fe6
kid101skater <kevin.locke90@gmail.com>
2018-02-25 14:11:12 -0800

    Add search field in admin page to search by a persons name (first and last)

--- IGrad/IGrad/Controllers/AdminController.cs
+++ IGrad/IGrad/Controllers/AdminController.cs
@@ -15,7 +15,7 @@ namespace IGrad.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index(string sortOrder)
+        public ViewResult Index(string sortOrder, string searchString)
         {
             using (UserContext db = new UserContext())
             {
@@ -25,6 +25,22 @@ namespace IGrad.Controllers
                     .Include(u => u.ConsideredRaceAndEthnicity)
                     .Include(u => u.PhoneInfo)
                     .ToList();
+
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    searchString = searchString.ToLower();
+                    var _tempFname = data.Where(s => s.Name.FName != null).ToList();
+                    var _tempLname = data.Where(s => s.Name.LName != null).ToList();
+
+                    _tempFname = _tempFname.Where(s => s.Name.FName.ToLower().Contains(searchString)).ToList();
+                    _tempLname = _tempFname.Where(s => s.Name.LName.ToLower().Contains(searchString)).ToList();
+
+                    var _tempList = new List<UserModel>();
+                    _tempList.AddRange(_tempFname);
+                    _tempList.Union(_tempLname);
+
+                    return View(_tempList);
+                }
 
                 return View(data);
             }
