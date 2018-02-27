9d7127a86feed0b92764fa2e5e8c52b63ca36875
ducknguyen <dt.nguyen95@yahoo.com>
2018-02-27 14:23:00 -0800

    Applying some styles to the application forms

--- IGrad/IGrad/Scripts/progress-bar.js
+++ IGrad/IGrad/Scripts/progress-bar.js
@@ -1,9 +1,12 @@
-﻿$(':input').change(function () {
+﻿$('#personal-info-form :input').change(function () {
     var count = 0;
     var totalRequiredFields = ($('.required-marker').length);
     console.log(totalRequiredFields);
 
-    $(':input[required]:visible').each(function () {
+    var totalRequiredFields1 = ($('#personal-info-form .form-group').length);
+    console.log(totalRequiredFields1);
+
+    $('#personal-info-form :input[required]:visible').each(function () {
         if ((this.validity.valid) == true) {
             count++;
             console.log(this);
@@ -13,4 +16,18 @@
     var calculateProgress = (count / totalRequiredFields) * 100;
     console.log(calculateProgress);
     progress.attr("value", calculateProgress);
+});
+//Adds student race to visible list on race selection/de-selection
+$(".raceCheck").change(function () {
+    // If checked
+    var value = $(this).val(),
+        $list = $("#studentRaceList");
+    if (this.checked) {
+        //add to the right
+        $list.append("<li data-value='" + value + "'>" + value + "<input type='hidden' name='studentRace[]' value='" + value + "'/></li>");
+    }
+    else {
+        //hide to the right
+        $list.find('li[data-value="' + value + '"]').remove();
+    }
 });
