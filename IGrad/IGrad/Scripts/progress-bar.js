6cffc235de2473ed24acad25d0bc9426dfadfc94
ducknguyen <dt.nguyen95@yahoo.com>
2018-02-27 08:24:50 -0800

    updated nav and footer for form pages

---
+++ IGrad/IGrad/Scripts/progress-bar.js
@@ -0,0 +1,16 @@
+﻿$(':input').change(function () {
+    var count = 0;
+    var totalRequiredFields = ($('.required-marker').length);
+    console.log(totalRequiredFields);
+
+    $(':input[required]:visible').each(function () {
+        if ((this.validity.valid) == true) {
+            count++;
+            console.log(this);
+        }
+    });
+    var progress = $("#application-progress");
+    var calculateProgress = (count / totalRequiredFields) * 100;
+    console.log(calculateProgress);
+    progress.attr("value", calculateProgress);
+});
