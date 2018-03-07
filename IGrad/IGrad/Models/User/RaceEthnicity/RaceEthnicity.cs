f2d5c700525bf138c9639e21488eb0318a9e83c8
ducknguyen <dt.nguyen95@yahoo.com>
2018-03-07 11:43:42 -0800

    Script to generate model methods and checkboxes in new application worked and is included. Waiting on Kevin's fix

--- IGrad/IGrad/Models/User/RaceEthnicity/RaceEthnicity.cs
+++ IGrad/IGrad/Models/User/RaceEthnicity/RaceEthnicity.cs
@@ -12,26 +12,40 @@ namespace IGrad.Models.User
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int fieldId { get; set; }
         public Guid UserID { get; set; }
-        [DisplayName("Not Hispanic Or Latino")]
-        public bool isNotHispanicOrLatino { get; set; }
-        [DisplayName("Cuban")]
-        public bool isCuban { get; set; }
-        [DisplayName("Dominican")]
-        public bool isDominican { get; set; }
-        [DisplayName("Spanish")]
-        public bool isSpaniard { get; set; }
-        [DisplayName("Puerto RIcan")]
-        public bool isPuertoRican { get; set; }
-        [DisplayName("Mexican")]
-        public bool isMexican { get; set; }
-        [DisplayName("Central American")]
-        public bool isCentralAmerican { get; set; }
-        [DisplayName("South American")]
-        public bool isSouthAmerican { get; set; }
-        [DisplayName("Latin American")]
-        public bool isLatinAmerican { get; set; }
-        [DisplayName("Other Hispanic / Latino")]
-        public bool isOtherHispanicLatino { get; set; }
+
+        //ASIAN -------------------------------------------------------------
+        [DisplayName("Asian Indian")]
+        public bool isAsianIndian { get; set; }
+        [DisplayName("Cambodian")]
+        public bool isCambodian { get; set; }
+        [DisplayName("Chinese")]
+        public bool isChinese { get; set; }
+        [DisplayName("Filipino")]
+        public bool isFilipino { get; set; }
+        [DisplayName("Hmong")]
+        public bool isHmong { get; set; }
+        [DisplayName("Indonesian")]
+        public bool isIndonesian { get; set; }
+        [DisplayName("Japanese")]
+        public bool isJapanese { get; set; }
+        [DisplayName("Korean")]
+        public bool isKorean { get; set; }
+        [DisplayName("Laotian")]
+        public bool isLaotian { get; set; }
+        [DisplayName("Malaysian")]
+        public bool isMalaysian { get; set; }
+        [DisplayName("Pakistani")]
+        public bool isPakistani { get; set; }
+        [DisplayName("Singaporean")]
+        public bool isSingaporean { get; set; }
+        [DisplayName("Taiwanese")]
+        public bool isTaiwanese { get; set; }
+        [DisplayName("Thai")]
+        public bool isThai { get; set; }
+        [DisplayName("Vietnamese")]
+        public bool isVietnamese { get; set; }
+        [DisplayName("Other Asian")]
+        public bool isOtherAsian { get; set; }
 
     }
 }
