e74c7eacb8369887d9cd49c8a740a7ec27188682
kid101skater <kevin.locke90@gmail.com>
2018-03-18 16:09:45 -0700

    Added Database Drop program

---
+++ IGrad/DatabaseDropper/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Data;
+
+namespace DatabaseDropper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gathering Connection String");
+            string sqlConnectionString = "Data Source=184.168.194.75;Initial Catalog=iGrad;Integrated Security=False;User ID=iGradAdmin;Password=Ezj11c#6;";
+
+            Console.WriteLine("Obtaining SQL Commands");
+            // Read all the sql text
+            string sql = "";
+
+            using (FileStream strm = File.OpenRead("drop.sql"))
+            {
+                StreamReader reader = new StreamReader(strm);
+                sql = reader.ReadToEnd();
+            }
+
+            Console.WriteLine("Executing Connection and Query");
+            using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+            {
+                try
+                {
+
+                    Regex regex = new Regex("^GO", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    string[] lines = regex.Split(sql);
+
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    using (SqlCommand cmd = connection.CreateCommand())
+                    {
+                        cmd.Connection = connection;
+                        cmd.Transaction = transaction;
+
+                        foreach (string line in lines)
+                        {
+                            if (line.Length > 0)
+                            {
+                                cmd.CommandText = line;
+                                cmd.CommandType = CommandType.Text;
+
+                                try
+                                {
+                                    cmd.ExecuteNonQuery();
+                                }
+                                catch (SqlException)
+                                {
+                                    Console.WriteLine("Chances are Database does not Exist, or Sql Script is not up to date");
+                                    transaction.Rollback();
+                                    throw;
+                                }
+                            }
+                        }
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERROR: ");
+                    Console.WriteLine(ex.ToString());
+                }
+                connection.Close();
+            }
+                
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("-            FINISHED           -");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IGrad/DatabaseDropper/drop.sql
@@ -0,0 +1,222 @@
+﻿USE [iGrad]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.SchoolInfoes_SchoolInfo_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.Retaineds_Retainment_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.RaceEthnicities_ConsideredRaceAndEthnicity_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.QualifiedOrEnrolledInPrograms_QualifiedOrEnrolledInProgam_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.Phones_PhoneInfo_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.ParentPlans_StudentsParentingPlan_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.Names_Name_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.LivesWiths_LivesWith_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.LifeEvents_LifeEvent_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.LanguageHistories_LanguageHisory_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.Healths_HealthInfo_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.ChildCares_StudentChildCare_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.Celebrates_Celebrate_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.BirthPlaceLocations_BirthPlace_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.Addresses_ResidentAddress_fieldId]
+GO
+
+ALTER TABLE [dbo].[User] DROP CONSTRAINT [FK_dbo.User_dbo.Addresses_MailingAddress_fieldId]
+GO
+
+ALTER TABLE [dbo].[Siblings] DROP CONSTRAINT [FK_dbo.Siblings_dbo.User_UserID]
+GO
+
+ALTER TABLE [dbo].[SchoolInfoes] DROP CONSTRAINT [FK_dbo.SchoolInfoes_dbo.Violations_PreviousSchoolViolation_fieldId]
+GO
+
+ALTER TABLE [dbo].[SchoolInfoes] DROP CONSTRAINT [FK_dbo.SchoolInfoes_dbo.PriorEducations_PriorEducation_fieldId]
+GO
+
+ALTER TABLE [dbo].[PreSchools] DROP CONSTRAINT [FK_dbo.PreSchools_dbo.User_UserID]
+GO
+
+ALTER TABLE [dbo].[PreSchools] DROP CONSTRAINT [FK_dbo.PreSchools_dbo.Addresses_Address_fieldId]
+GO
+
+ALTER TABLE [dbo].[HighSchoolInfoes] DROP CONSTRAINT [FK_dbo.HighSchoolInfoes_dbo.SchoolInfoes_SchoolInfo_fieldId]
+GO
+
+ALTER TABLE [dbo].[Guardians] DROP CONSTRAINT [FK_dbo.Guardians_dbo.User_UserID]
+GO
+
+ALTER TABLE [dbo].[Guardians] DROP CONSTRAINT [FK_dbo.Guardians_dbo.Phones_Phone_fieldId]
+GO
+
+ALTER TABLE [dbo].[Guardians] DROP CONSTRAINT [FK_dbo.Guardians_dbo.Names_Name_fieldId]
+GO
+
+ALTER TABLE [dbo].[Guardians] DROP CONSTRAINT [FK_dbo.Guardians_dbo.Addresses_Address_fieldId]
+GO
+
+ALTER TABLE [dbo].[EmergencyContacts] DROP CONSTRAINT [FK_dbo.EmergencyContacts_dbo.User_UserID]
+GO
+
+ALTER TABLE [dbo].[EmergencyContacts] DROP CONSTRAINT [FK_dbo.EmergencyContacts_dbo.Phones_PhoneNumber_fieldId]
+GO
+
+ALTER TABLE [dbo].[EmergencyContacts] DROP CONSTRAINT [FK_dbo.EmergencyContacts_dbo.Names_Name_fieldId]
+GO
+
+ALTER TABLE [dbo].[ChildCares] DROP CONSTRAINT [FK_dbo.ChildCares_dbo.Addresses_ProviderAddress_fieldId]
+GO
+
+ALTER TABLE [dbo].[AspNetUserRoles] DROP CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId]
+GO
+
+ALTER TABLE [dbo].[AspNetUserRoles] DROP CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId]
+GO
+
+ALTER TABLE [dbo].[AspNetUserLogins] DROP CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId]
+GO
+
+ALTER TABLE [dbo].[AspNetUserClaims] DROP CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId]
+GO
+
+/****** Object:  Table [dbo].[Violations]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Violations]
+GO
+
+/****** Object:  Table [dbo].[User]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[User]
+GO
+
+/****** Object:  Table [dbo].[Siblings]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Siblings]
+GO
+
+/****** Object:  Table [dbo].[SchoolInfoes]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[SchoolInfoes]
+GO
+
+/****** Object:  Table [dbo].[Retaineds]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Retaineds]
+GO
+
+/****** Object:  Table [dbo].[RaceEthnicities]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[RaceEthnicities]
+GO
+
+/****** Object:  Table [dbo].[QualifiedOrEnrolledInPrograms]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[QualifiedOrEnrolledInPrograms]
+GO
+
+/****** Object:  Table [dbo].[PriorEducations]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[PriorEducations]
+GO
+
+/****** Object:  Table [dbo].[PreSchools]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[PreSchools]
+GO
+
+/****** Object:  Table [dbo].[Phones]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Phones]
+GO
+
+/****** Object:  Table [dbo].[ParentPlans]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[ParentPlans]
+GO
+
+/****** Object:  Table [dbo].[Names]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Names]
+GO
+
+/****** Object:  Table [dbo].[LivesWiths]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[LivesWiths]
+GO
+
+/****** Object:  Table [dbo].[LifeEvents]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[LifeEvents]
+GO
+
+/****** Object:  Table [dbo].[LanguageHistories]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[LanguageHistories]
+GO
+
+/****** Object:  Table [dbo].[HighSchoolInfoes]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[HighSchoolInfoes]
+GO
+
+/****** Object:  Table [dbo].[Healths]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Healths]
+GO
+
+/****** Object:  Table [dbo].[Guardians]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Guardians]
+GO
+
+/****** Object:  Table [dbo].[EmergencyContacts]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[EmergencyContacts]
+GO
+
+/****** Object:  Table [dbo].[ChildCares]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[ChildCares]
+GO
+
+/****** Object:  Table [dbo].[Celebrates]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Celebrates]
+GO
+
+/****** Object:  Table [dbo].[BirthPlaceLocations]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[BirthPlaceLocations]
+GO
+
+/****** Object:  Table [dbo].[AspNetUsers]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[AspNetUsers]
+GO
+
+/****** Object:  Table [dbo].[AspNetUserRoles]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[AspNetUserRoles]
+GO
+
+/****** Object:  Table [dbo].[AspNetUserLogins]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[AspNetUserLogins]
+GO
+
+/****** Object:  Table [dbo].[AspNetUserClaims]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[AspNetUserClaims]
+GO
+
+/****** Object:  Table [dbo].[AspNetRoles]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[AspNetRoles]
+GO
+
+/****** Object:  Table [dbo].[Addresses]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[Addresses]
+GO
+
+/****** Object:  Table [dbo].[__MigrationHistory]    Script Date: 3/18/2018 3:27:00 PM ******/
+DROP TABLE [dbo].[__MigrationHistory]
+GO
+
+
