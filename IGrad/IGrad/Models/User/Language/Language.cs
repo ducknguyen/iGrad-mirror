0d0e6b65ca75bf738f20389864c7efb220810562
ducknguyen <dt.nguyen95@yahoo.com>
2018-03-19 14:49:45 -0700

    FInalized language form, updated language model

--- IGrad/IGrad/Models/User/Language/Language.cs
+++ IGrad/IGrad/Models/User/Language/Language.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,13 +12,31 @@ namespace IGrad.Models.User
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int fieldId { get; set; }
         public Guid UserID { get; set; }
+
+        [DisplayName("1. In what language(s) would your family prefer to communicate with school?")]
         public string PreferredLanguage { get; set; }
+
+        [DisplayName("2. What language did the student learn first?")]
         public string UserFirstLanguageLearned { get; set; }
+
+        [DisplayName("3. What language does the student use primarily at home?")]
         public string StudentPrimaryLanguageAtHome { get; set; }
+
+        [DisplayName("4. What is the primary language used in the home, regardless of the language spoken by the student?")]
         public string PrimaryLanguageSpokenAtHome { get; set; }
+
+        [DisplayName("5. Has the student received English language development supporting a previous school?")]
         public bool StudentReceievedEnglishDevelopmentSupport {get;set;}
+
         public bool unsureOfEnglishSupport { get; set; } // for Student recieved eng dev support, they have an unknown option
+
+
+        [DisplayName("6. Has the student ever received formal education outside of the United States?")]
         public bool StudentHasReceivedFormalEducationOutsideUS { get; set; }
-        public int StudentStartingGradeInUS { get; set; }
+
+        //need to add months and language of instruction
+
+        [DisplayName("7. When did the student first attend a school in the United States?")]
+        public string StudentStartingGradeInUS { get; set; }
     }
 }
