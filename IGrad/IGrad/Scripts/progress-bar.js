81481445291303dfcbf85a797d372d785570b31b
ducknguyen <dt.nguyen95@yahoo.com>
2018-04-18 11:16:11 -0700

    tooltip is working probably and added required-marker in the main forms

--- IGrad/IGrad/Scripts/progress-bar.js
+++ IGrad/IGrad/Scripts/progress-bar.js
@@ -1,42 +1,82 @@
-﻿$(document).ready(function () {
-    var count = 0;
-    var totalRequiredFields = ($('.required-marker').length);
+﻿
+var count = 0;
 
-    $(':input[required]:visible').each(function () {
-        if ((this.validity.valid) == true) {
-            count++;
-        }
-    });
-    var progress = $("#application-progress");
-    var calculateProgress = (count / totalRequiredFields) * 100;
-    progress.attr("value", calculateProgress);
-});
-$(':input').change(function () {
-
-    var count = 0;
-    var totalRequiredFields = ($('.required-marker').length);
-
-    $(':input[required]:visible').each(function () {
-        if ((this.validity.valid) == true) {
-            count++;
-        }
-    });
-    var progress = $("#application-progress");
-    var calculateProgress = (count / totalRequiredFields) * 100;
-    progress.attr("value", calculateProgress);
+$(document).ready(function () {
+    checkProgress();
+    checkRadioButtonProgress();
 });
 
-//Adds student race to visible list on race selection/de-selection
-$(".raceCheck").change(function () {
-    // If checked
-    var value = $(this).val(),
-        $list = $("#studentRaceList");
-    if (this.checked) {
-        //add to the right
-        $list.append("<li data-value='" + value + "'>" + value + "<input type='hidden' name='studentRace[]' value='" + value + "'/></li>");
+$('.required-checker').change(function () {
+/*    if ($(this).val().length > 0) {
+        count++;
+    }
+    else if ($(this).children().length > 0) {
+        if ($(this).children(":first").val().length > 0) {
+            count++;
+        }
     }
     else {
-        //hide to the right
-        $list.find('li[data-value="' + value + '"]').remove();
+        count--;
+    }*/
+    if (this.validity.valid) {
+        count++
     }
+    console.log("checker " + count);
+    updateProgressBar(count);
 });
+
+$(':input[type="radio"]').change(function () {
+    //checkRadioButtonProgress();
+    //count--;
+    if (this.validity.valid) {
+        count++
+    }
+    updateProgressBar(count);
+    console.log("radio " + count);
+});
+
+// CHECK INITIAL PROGRESS OF ALL REQUIRED-CHECKERS AND RADIO-BUTTONS 
+function checkProgress() {
+    $('.required-checker').each(function () {
+        if ($(this).val().length > 0) {
+            count++;
+        }
+        else if ($(this).children().length > 0) {
+            if ($(this).children(":first").val().length > 0) {
+                count++;
+            }
+        }
+    });
+    updateProgressBar(count);
+}
+
+function checkRadioButtonProgress() {
+    $(':input[type="radio"]').each(function () {
+        if ($(this).is(':checked')) {
+            count++;
+        }
+    });
+    updateProgressBar(count);
+}
+
+
+$(":input").keyup(function () {
+    var count = 0;
+    $(":input .required-checker").each(function () {
+        if (this.validity.valid) {
+            count++;
+        }
+    });
+    updateProgressBar(count);
+});
+
+
+// calculate and update progress bar base on count
+function updateProgressBar(count) {
+    var totalRequiredFields = ($('.required-marker').length);
+    var progress = $("#application-progress");
+    var calculateProgress = (count / totalRequiredFields) * 100;
+
+    console.log("in progress update " + calculateProgress + " " + totalRequiredFields + " " + count)
+    progress.attr("value", calculateProgress);
+}
