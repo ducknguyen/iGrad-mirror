b9b0781187ceec4683bb261de5f66a3c21cbf620
kid101skater <kevin.locke90@gmail.com>
2018-04-30 20:02:30 -0700

    Add Military Info to user model

---
+++ IGrad/IGrad/Models/User/Millitary/MillitaryInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IGrad.Models.User
+{
+    public class MillitaryInfo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int fieldId { get; set; }
+
+        public bool ArmedForcesActiveDuty { get; set; }
+        public bool ArmedForcesReserved { get; set; }
+        public bool NationalGuard { get; set; }
+        public bool MoreThanOne { get; set; }
+        public bool None { get; set; }
+        public bool PreferNotToAnswer { get; set; }
+    }
+}
