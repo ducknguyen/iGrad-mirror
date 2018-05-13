7c140e63d81b857f1f517b574c713723b7459798
JEFF <J.PRATT3991@GMAIL.COM>
2018-05-13 11:16:18 -0700

    Added displayname attributes to lives with model properties

--- IGrad/IGrad/Models/User/LivesWith/LivesWithList.cs
+++ IGrad/IGrad/Models/User/LivesWith/LivesWithList.cs
@@ -11,15 +11,25 @@ namespace IGrad.Models.User
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int fieldId { get; set; }
         public Guid UserID { get; set; }
+        [DisplayName("Student Lives With Both Parents")]
         public bool LivesWithBothParents { get; set; }
+        [DisplayName("Student Lives With Guardian")]
         public bool LivesWithGuardian { get; set; }
+        [DisplayName("Student Lives With Mother Only")]
         public bool LivesWithMotherOnly { get; set; }
+        [DisplayName("Student Lives With Father Only")]
         public bool LivesWithFatherOnly { get; set; }
+        [DisplayName("Student Lives With Grandparents")]
         public bool LivesWithGrandparents { get; set; }
+        [DisplayName("Student Provides Own Housing ")]
         public bool LivesWithSelf { get; set; }
+        [DisplayName("Student Lives With Father and Step Mother")]
         public bool LivesWithFatherAndStepMom { get; set; }
+        [DisplayName("Student Lives With Mother and Step Father")]
         public bool LivesWithMotherAndStepDad { get; set; }
+        [DisplayName("Student Lives With Foster Parents")]
         public bool LivesWithFosterParents { get; set; }
+        [DisplayName("Student Housing Provided By Agency")]
         public bool LivesWithAgency { get; set; }
         [DisplayName("Homeless / Distressed")]
         public bool HomelessDistressed { get; set; }
@@ -27,6 +37,7 @@ namespace IGrad.Models.User
         public int TotalPeopleInResidence { get; set; }
         [DisplayName("Annual Household Income")]
         public int AnnualHouseHoldIncome { get; set; }
+        [DisplayName("Name of Agency Providing Housing")]
         public string AgencyName { get; set; }
         public string Other { get; set; }
     }
