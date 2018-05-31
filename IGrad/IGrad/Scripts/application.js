a93fd3a2e9d4b61bb5944373868045c5c3aad9cf
ducknguyen <dt.nguyen95@yahoo.com>
2018-05-31 01:39:25 -0700

    Added blur effect on form when a row is focused

--- IGrad/IGrad/Scripts/application.js
+++ IGrad/IGrad/Scripts/application.js
@@ -5,3 +5,18 @@
 function showTarget(targetId) {
     $('#' + targetId).removeClass('hidden');
 }
+
+$("input:not(.race-checkbox)").focus(function(){
+    console.log("inside focus");
+    var parent = $(this).parent().closest('.row');
+    $('.row').addClass("blur-content");
+    parent.removeClass("blur-content");
+    parent.addClass("clear-content");
+});
+
+$("input:not(.race-checkbox)").focusout(function(){
+    console.log("inside focusout");
+    var parent = $(this).parent().closest('.row');
+    $('.row').removeClass("blur-content");
+    parent.removeClass("clear-content");
+});
